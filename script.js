a4540e6f5267f794f93993e07df89decb49184f9
Kai Heale <cludder@hotmail.co.uk>
2021-02-24 23:59:12 +0000

    Add DOM update functionality

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+function durham() {
+    headline = document.getElementById("headline");
+    headline.innerHTML = "1st Year Computer Science BSc @ Durham University"
+
+    para1 = document.getElementById("p1");
+    para1.innerHTML = "On track for a 1st Class degree"
+
+    para2 = document.getElementById("p2");
+    para2.innerHTML = "180 Degrees Consulting, Consulting Society, CompSoc, Mary's RAOKS, Butler Jazz, SMCFC, DU Fencing"
+
+    document.body.style.backgroundImage = 'url("assets/marys.png")';
+}
+
 function voluntech() {
     headline = document.getElementById("headline");
-    headline.innerHTML = "VOLUNTECH";
+    headline.innerHTML = "Founder and Director of Voluntech";
+
+    para1 = document.getElementById("p1");
+    para1.innerHTML = "Many charities struggle to properly utilise technology to improve their operations. Voluntech connects them to skilled student developers to help them solve these problems; in return, students gain experience of working on real-life, real impact projects."
+
+    para2 = document.getElementById("p2");
+    para2.innerHTML = "Past clients: Change A Child's Life, Hullo"
+
+    document.body.style.backgroundColor = "#8c20ba";
+    document.body.style.backgroundImage = "none";
 }
