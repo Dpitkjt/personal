ecc4489e8d9186a0d1bb76b9d60217458cec6068
Kai Heale <cludder@hotmail.co.uk>
2021-02-25 00:21:47 +0000

    Add further links

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function durham() {
     headline.innerHTML = "1st Year Computer Science BSc @ Durham University"
 
     para1 = document.getElementById("p1");
-    para1.innerHTML = "On track for a 1st Class degree"
+    para1.innerHTML = "On track for a 1st Class degree. Spring Intern at Morgan Stanley and mThree."
 
     para2 = document.getElementById("p2");
-    para2.innerHTML = "180 Degrees Consulting, Consulting Society, CompSoc, Mary's RAOKS, Butler Jazz, SMCFC, DU Fencing"
+    para2.innerHTML = "180 Degrees Consulting, Consulting Society, CompSoc, Mary's RAOKS, Butler Jazz, SMCFC, DU Fencing, DUES, POVD Department Rep"
 
     document.body.style.backgroundImage = 'url("assets/marys.png")';
 }
@@ -16,11 +16,25 @@ function voluntech() {
     headline.innerHTML = "Founder and Director of Voluntech";
 
     para1 = document.getElementById("p1");
-    para1.innerHTML = "Many charities struggle to properly utilise technology to improve their operations. Voluntech connects them to skilled student developers to help them solve these problems; in return, students gain experience of working on real-life, real impact projects."
+    para1.innerHTML = "Voluntech connects charities and social enterprises to skilled student developers to help them solve tech problems; in return, students gain experience of working on real-life, real impact projects."
 
     para2 = document.getElementById("p2");
-    para2.innerHTML = "Past clients: Change A Child's Life, Hullo"
+    para2.innerHTML = "Past clients: Change A Child's Life, <a href='http://hullo.org'>Hullo</a>"
 
     document.body.style.backgroundColor = "#8c20ba";
+    document.body.style.backgroundImage = "none";
+}
+
+function droppr() {
+    headline = document.getElementById("headline");
+    headline.innerHTML = "Web Developer at <a href='http://dropprgame.com'>Droppr</a>";
+
+    para1 = document.getElementById("p1");
+    para1.innerHTML = "We hide prizes, you find them! Droppr is a student start-up allowing users to win prizes, including cash and vouchers sponsored by local businesses, by taking part in interactive scavenger hunts."
+
+    para2 = document.getElementById("p2");
+    para2.innerHTML = "Projects: Website, QR code promo, admin panel"
+
+    document.body.style.backgroundColor = "#00C853";
     document.body.style.backgroundImage = "none";
 }
